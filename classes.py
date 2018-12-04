1fd527b3f26811ae99b5220c3081077f64de644e
mishakh <45524887+mishakh@users.noreply.github.com>
2018-12-04 16:43:00 -0500

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,42 @@
+
+
+
+class Car:
+
+    def __init__(self):
+        self.__wheels = None
+        self.__engine = None
+        self.__body = None
+        self.__transmission = None
+
+
+    carMass = None
+    enginePower = None
+    surfaceArea = None
+    maxSpeed = None
+    acceleration = None
+
+
+class fastCar(Car):
+
+
+
+class bigCar(Car):
+
+
+
+class rocketCar(Car):
+
+
+class Engine:
+    horsepower = None
+
+class Wheels:
+    size = None
+
+class Body:
+    airDrag = None
+
+class Transmission:
+    gearbox = None
+

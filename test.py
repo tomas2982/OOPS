7b2ed785c12065fb4561695527ce19f3c8c2f93c
mishakh <45524887+mishakh@users.noreply.github.com>
2018-12-06 19:53:15 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from main import *
+from classes import *
 """
 raceCar1 = Car(Wheels(18, 2, 70),Engine(250, 3, .3, 4750, 325),Body(.2, 1.3))
 raceCar2 = Car(Wheels(19, 3, 80),Engine(200, 2, .3, 4500, 350),Body(.19, 1.4))
@@ -24,20 +24,55 @@ carList=[raceCar1,raceCar2,zoomBoy,fatBoy,PCPBABY]
 winnersList = []
 for x in carList:
     winnersList.append(x.getFinishTime())
-
 winner = carList[winnersList.index(min(winnersList))]
-
 print(winner)
 """
 
-myCar1 = Car(Wheels(18, 2, 70),Engine(250, 3, .3, 4750, 325),Body(.2, 1.3))
-enemyCar1 = Car(Wheels(random.randint(18,22),random.randint(0,5),random.randint(60,70))
-,Engine(random.randint(100,200),random.randint(0,5),random.uniform(1.0,1.8),random.randint(4000,4200),random.randint(0,100))
-,Body(random.uniform(0.3,0.4),random.uniform(2.0,2.5)))
+while 1:
+    print("Choose 1 for Easy, 2 For Medium, 3 for Hard")
+    choice = input("Difficulty you want: ")
+    choice = choice
+    if 1 == choice:
+        print("WHYARENTYOUDOINGANYTHING")
+        myCar1 = Car(Wheels(18, 2, 70), Engine(250, 3, .3, 4750, 325), Body(.2, 1.3))
 
-print("myCar1")
-myCar1.printSpecs()
-print("max accel: ", myCar1.getAccel(), " m/s^2 Finish time: ", myCar1.getFinishTime(), "seconds")
-print("enemyCar1")
-enemyCar1.printSpecs()
-print("max accel: ", enemyCar1.getAccel(), " m/s^2 Finish time: ", enemyCar1.getFinishTime(), "seconds")
+        enemyCar1 = Car(Wheels(random.randint(18, 20), random.randint(0, 5), random.randint(60, 70))
+                        , Engine(random.randint(100, 200), random.randint(0, 5), random.uniform(1.0, 1.8),
+                                 random.randint(4000, 4200), random.randint(0, 100))
+                        , Body(random.uniform(0.3, 0.4), random.uniform(2.0, 2.5)))
+        print("myCar1")
+        myCar1.printSpecs()
+        print("max accel: ", myCar1.getAccel(), " m/s^2 Finish time: ", myCar1.getFinishTime(), "seconds")
+        print("enemyCar1")
+        enemyCar1.printSpecs()
+        print("max accel: ", enemyCar1.getAccel(), " m/s^2 Finish time: ", enemyCar1.getFinishTime(), "seconds")
+
+    elif choice == 2:
+        myCar1 = Car(Wheels(18, 2, 70), Engine(250, 3, .3, 4750, 325), Body(.2, 1.3))
+
+        enemyCar1 = bigCar(Wheels(random.randint(19, 22), random.randint(0, 4), random.randint(65, 75))
+                        , Engine(random.randint(175, 250), random.randint(0, 4), random.uniform(1.0, 1.7),
+                                 random.randint(4200, 4500), random.randint(0, 100))
+                        , Body(random.uniform(0.2, 0.3), random.uniform(1.5, 2.0)))
+        print("myCar1")
+        myCar1.printSpecs()
+        print("max accel: ", myCar1.getAccel(), " m/s^2 Finish time: ", myCar1.getFinishTime(), "seconds")
+        print("enemyCar1")
+        enemyCar1.printSpecs()
+        print("max accel: ", enemyCar1.getAccel(), " m/s^2 Finish time: ", enemyCar1.getFinishTime(), "seconds")
+
+    elif choice == 3:
+        myCar1 = Car(Wheels(18, 2, 70), Engine(250, 3, .3, 4750, 325), Body(.2, 1.3))
+
+        enemyCar1 = Car(Wheels(random.randint(20, 23), random.randint(0, 3), random.randint(70, 80))
+                        , Engine(random.randint(100, 200), random.randint(0, 5), random.uniform(1.0, 1.8),
+                                 random.randint(4500, 4750), random.randint(0, 100))
+                        , Body(random.uniform(0.2, 0.25), random.uniform(1.5, 1.8)))
+        print("myCar1")
+        myCar1.printSpecs()
+        print("max accel: ", myCar1.getAccel(), " m/s^2 Finish time: ", myCar1.getFinishTime(), "seconds")
+        print("enemyCar1")
+        enemyCar1.printSpecs()
+        print("max accel: ", enemyCar1.getAccel(), " m/s^2 Finish time: ", enemyCar1.getFinishTime(), "seconds")
+
+print("end of game")
